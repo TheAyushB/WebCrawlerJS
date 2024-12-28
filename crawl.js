694d7e97380fdb7f08f3be9d0d7ec20bb2a23e02
TheAyushB <ayushbhardwaj510@gmail.com>
2024-12-28 14:30:59 +0530

    getURLsFromHTML

--- crawl.js
+++ crawl.js
@@ -1,3 +1,17 @@
+const {JSDOM} = require('jsdom');
+
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+
+    for (const linkElement of linkElements) {
+        urls.push(linkElement.href);
+        
+    }
+    return urls;
+}
 
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString);
@@ -11,5 +25,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,32 +1,48 @@
 
-const {normalizeURL} = require('./crawl.js')
-const {test, expect} = require('@jest/globals')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
 
 test('normalizeURL strip protocol', () => {
-    const input = 'https://blog.ayush.ab/dev';
+    const input = 'https://blog.ayush.dev/path';
     const actual = normalizeURL(input);
-    const expected = 'blog.ayush.ab/dev';
+    const expected = 'blog.ayush.dev/path';
     expect(actual).toEqual(expected);
 })
 
 test('normalizeURL trailing slash', () => {
-    const input = 'https://blog.ayush.ab/dev/';
+    const input = 'https://blog.ayush.dev/path/';
     const actual = normalizeURL(input);
-    const expected = 'blog.ayush.ab/dev';
+    const expected = 'blog.ayush.dev/path';
     expect(actual).toEqual(expected);
 })
 
 test('normalizeURL capitals', () => {
-    const input = 'https://blog.AYUSH.ab/dev/';
+    const input = 'https://blog.AYUSH.dev/path/';
     const actual = normalizeURL(input);
-    const expected = 'blog.ayush.ab/dev';
-    expect(actual).toEqual(expected);  
+    const expected = 'blog.ayush.dev/path';
+    expect(actual).toEqual(expected);
 })
 
 test('normalizeURL strip http', () => {
-    const input = 'http://blog.ayush.ab/dev/';
+    const input = 'http://blog.ayush.dev/path/';
     const actual = normalizeURL(input);
-    const expected = 'blog.ayush.ab/dev';
-    expect(actual).toEqual(expected);  
+    const expected = 'blog.ayush.dev/path';
+    expect(actual).toEqual(expected);
+})
+
+test('getURLsFromHTML', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://blog.ayush.dev/">
+                Ayush.dev Blog
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.ayush.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+    const expected = ["https://blog.ayush.dev/"];
+    expect(actual).toEqual(expected);
 })
 
